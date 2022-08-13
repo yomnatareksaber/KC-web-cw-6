f434ab4ef59879615ee71d53a9574cfd4e0f06ed
yomnatareksaber <yomnaaaat@gmail.com>
2022-08-13 14:06:16 +0300

    day6 :)

---
+++ page.js
@@ -0,0 +1,23 @@
+
+
+alert("wellcome to my website that is going to Calculates your marks") 
+let grade = prompt ("اكتب درجتك بالارقام")
+console.log(grade);
+if(grade>=90) {
+    console.log("لقد حصلت على امتياز 🥳");
+} else if (grade>=80){
+    console.log("لقد حصلت على جيد جداً🤩");
+} else if (grade>=70){
+    console.log("لقد حصلت على جيد🙂");
+} else if (grade>=60){
+    console.log("لقد حصلت على مقبول😕");
+} else if (grade>=50){
+    console.log("لقد حصلت على ضعيف☹️");
+} else if (grade<50) {
+console.log("راسب💔");
+} 
+
+//hello 
+// This is an example of single line comments
+
+// جافاسكريبت ليست جافا
